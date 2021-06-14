10d62d28d0a2852a05a91a9df83eb598fe6b8655
FilipGaida99 <42692696+BoomboxPL1@users.noreply.github.com>
2021-06-14 14:45:15 +0200

    Usluga kar

---
+++ BibliotekaModel/KaraFunctions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Biblioteka
+{
+    public partial class Kara
+    {
+        public static void Refresh(BibliotekaDB db)
+        {
+            var now = DateTime.Now;
+            foreach(var reader in db.Czytelnik)
+            {
+                foreach(var lend in reader.Wypożyczenie)
+                {
+                    if(!lend.Ended && lend.Przewidywany_zwrot < now && !lend.Kara.All(p => p.Data_amnestii.HasValue))
+                    {
+                        lend.Kara.Add(new Kara { Data_nałożenia = now });
+                        //TODO: powiadomienie.
+                    }
+                }
+            }
+            db.SaveChanges();
+        }
+
+    }
+
+}
---
+++ BibliotekaService/Service1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteka;
+using System.Timers;
+using System.IO;
+
+namespace BibliotekaService
+{
+    public partial class Service1 : ServiceBase
+    {
+        const int hoursInterval = 4;
+        static readonly string configFilePath = AppDomain.CurrentDomain.BaseDirectory + "\\Config.txt";
+        public Service1()
+        {
+            InitializeComponent();
+            eventLog = new EventLog();
+            if (!EventLog.SourceExists("BibliotekaPubliczna"))
+            {
+                EventLog.CreateEventSource(
+                    "BibliotekaPubliczna", "BibliotekaLog");
+            }
+            eventLog.Source = "BibliotekaPubliczna";
+            eventLog.Log = "BibliotekaLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog.WriteEntry("Start.");
+
+            if(!File.Exists(AppDomain.CurrentDomain.BaseDirectory + "\\Config.txt"))
+            {
+                CreateNewConfigFile();
+            }
+
+            RefreshLibrary();
+            Timer timer = new Timer();
+            timer.Interval = hoursInterval * 60 * 60 * 1000;
+            timer.Elapsed += new ElapsedEventHandler(OnTimer);
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            eventLog.WriteEntry("Stop.");
+        }
+
+        public void OnTimer(object sender, ElapsedEventArgs args)
+        {
+            eventLog.WriteEntry("Odswiezanie kar i powiadomien biblioteki", EventLogEntryType.Information);
+            RefreshLibrary();
+        }
+
+        private void RefreshLibrary()
+        {
+            using (var db = new BibliotekaDB())
+            {
+                Kara.Refresh(db);
+                //TODO rozsyłanie maili
+            }
+        }
+
+        private void CreateNewConfigFile()
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(configFilePath, false);
+                sw.WriteLine("Email:");
+                sw.WriteLine("Pasword:");
+                sw.WriteLine("Server:");
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+                eventLog.WriteEntry("Blad tworzenia pliku konfiguracyjnego", EventLogEntryType.Information);
+            }
+        }
+    }
+}
