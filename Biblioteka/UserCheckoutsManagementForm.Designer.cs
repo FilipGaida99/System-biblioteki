8cb3abe9957a73c0d9874f7220794479e36a5e03
Donkrzawayan <43960303+Donkrzawayan@users.noreply.github.com>
2021-06-16 14:00:15 +0200

    manual extension on the librarian's side

--- Biblioteka/UserCheckoutsManagementForm.Designer.cs
+++ Biblioteka/UserCheckoutsManagementForm.Designer.cs
@@ -30,25 +30,40 @@ namespace Biblioteka
         private void InitializeComponent()
         {
             this.returnButton = new System.Windows.Forms.Button();
+            this.extensionButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // returnButton
             // 
-            this.returnButton.Location = new System.Drawing.Point(238, 9);
-            this.returnButton.Name = "button1";
+            this.returnButton.Location = new System.Drawing.Point(163, 9);
+            this.returnButton.Name = "returnButton";
             this.returnButton.Size = new System.Drawing.Size(75, 23);
             this.returnButton.TabIndex = 5;
             this.returnButton.Text = "Zwróć";
             this.returnButton.UseVisualStyleBackColor = true;
             this.returnButton.Click += new System.EventHandler(this.returnButton_Click);
             // 
+            // extensionButton
+            // 
+            this.extensionButton.Location = new System.Drawing.Point(244, 9);
+            this.extensionButton.Name = "extensionButton";
+            this.extensionButton.Size = new System.Drawing.Size(75, 23);
+            this.extensionButton.TabIndex = 6;
+            this.extensionButton.Text = "Prolonguj";
+            this.extensionButton.UseVisualStyleBackColor = true;
+            this.extensionButton.Click += new System.EventHandler(this.extensionButton_Click);
+            // 
             // UserCheckoutsManagementForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.ClientSize = new System.Drawing.Size(664, 297);
+            this.Controls.Add(this.extensionButton);
             this.Controls.Add(this.returnButton);
             this.Name = "UserCheckoutsManagementForm";
+            this.Controls.SetChildIndex(this.checkoutsList, 0);
+            this.Controls.SetChildIndex(this.returnedCheckBox, 0);
             this.Controls.SetChildIndex(this.returnButton, 0);
+            this.Controls.SetChildIndex(this.extensionButton, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,5 +72,6 @@ namespace Biblioteka
         #endregion
 
         private System.Windows.Forms.Button returnButton;
+        private System.Windows.Forms.Button extensionButton;
     }
 }
