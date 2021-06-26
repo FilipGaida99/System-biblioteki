74d6e4ff18a0869d4ae547706aba87ad0ee9bf38
FilipGaida99 <42692696+BoomboxPL1@users.noreply.github.com>
2021-06-26 19:24:55 +0200

    merge raporty cleanup

--- BibliotekaModel/Wiadomość.cs
+++ BibliotekaModel/Wiadomość.cs
@@ -60,5 +60,45 @@ namespace Biblioteka
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Czytelnik_Wiadomość> Czytelnik_Wiadomość { get; set; }
 
+        /// <summary>
+        /// Wysyłanie wiadomości jako biblioteka.
+        /// </summary>
+        /// <param name="title">Tytuł wiadomości.</param>
+        /// <param name="content">Treść wiadomości.</param>
+        /// <param name="addressee">Czytelnik, do którego wysyłana jest wiadomość.</param>
+        public static void SystemNotification(string title, string content, Czytelnik addressee)
+        {
+            Wiadomość msg = new Wiadomość { Treść = content, Tytuł = title, Data_wysłania = DateTime.Now };
+            using (var db = new BibliotekaDB())
+            {
+                db.Wiadomość.Add(msg);
+                Bibliotekarz librarian = db.Bibliotekarz
+                                        .Where(lib => lib.Imię == Bibliotekarz.specialLibrarianName)
+                                        .First();
+                db.Czytelnik_Wiadomość.Add(new Czytelnik_Wiadomość { CzytelnikID = addressee.CzytelnikID, Nadawca = false, Wiadomość = msg });
+                db.Bibliotekarz_Wiadomość.Add(new Bibliotekarz_Wiadomość { BibliotekarzID = librarian.BibliotekarzID, Nadawca = true, Wiadomość = msg });
+            }
+        }
+
+        /// <summary>
+        /// Wysyłanie wiadomości jako biblioteka.
+        /// </summary>
+        /// <param name="title">Tytuł wiadomości.</param>
+        /// <param name="content">Treść wiadomości.</param>
+        /// <param name="addresseeID">Identyfikator czytelnika.</param>
+        public static void SystemNotification(string title, string content, long addresseeID)
+        {
+            Wiadomość msg = new Wiadomość { Treść = content, Tytuł = title, Data_wysłania = DateTime.Now };
+            using (var db = new BibliotekaDB())
+            {
+                db.Wiadomość.Add(msg);
+                Bibliotekarz librarian = db.Bibliotekarz
+                                        .Where(lib => lib.Imię == Bibliotekarz.specialLibrarianName)
+                                        .First();
+                db.Czytelnik_Wiadomość.Add(new Czytelnik_Wiadomość { CzytelnikID = addresseeID, Nadawca = false, Wiadomość = msg });
+                db.Bibliotekarz_Wiadomość.Add(new Bibliotekarz_Wiadomość { BibliotekarzID = librarian.BibliotekarzID, Nadawca = true, Wiadomość = msg });
+                db.SaveChanges();
+            }
+        }
     }
 }
